a942a8de2400f706cd8b9188f87d986b83815938
MeatyHarvey <harveyvincent1@gmail.com>
2022-07-23 18:39:28 -0400

    refacto

--- PythonApp/BookFastApi/books.py
+++ PythonApp/BookFastApi/books.py
@@ -5,38 +5,43 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 
 BOOKS = {
-    1: { 'title': 'Title One', 'author': 'Auhtor 1'},
-    2: { 'title': 'Title Two', 'author': 'Auhtor 2'},
-    3: { 'title': 'Title Three', 'author': 'Auhtor 3'},
-    4: { 'title': 'Title Four', 'author': 'Auhtor 4'},
-    5: { 'title': 'Title Five', 'author': 'Auhtor 5'}    
+    1: {'title': 'Title One', 'author': 'Author 1'},
+    2: {'title': 'Title Two', 'author': 'Author 2'},
+    3: {'title': 'Title Three', 'author': 'Author 3'},
+    4: {'title': 'Title Four', 'author': 'Author 4'},
+    5: {'title': 'Title Five', 'author': 'Author 5'}
 }
+
 
 # GET
 @app.get("/")
-async def read_all_books(skip_book_id: Optional[int] = None) -> str:
+async def read_all_books(skip_book_id: Optional[int] = None) -> dict[int, dict[str, str]]:
     if skip_book_id:
         new_books = BOOKS.copy()
         del new_books[skip_book_id]
         return new_books
     return BOOKS
 
+
 # Basic GET
-@app.get("/books/mybook")
-async def read_favorite_book() -> str:
-    return { 'book_title': 'My favorite book' }
+@app.get("/books/my_book")
+async def read_favorite_book() -> dict[str, str]:
+    return {'book_title': 'My favorite book'}
+
 
 # Query Parameter GET
 @app.get("/books/")
-async def read_book(book_id: int) -> str:
+async def read_book(book_id: int) -> dict[str, str]:
     validate_book_id(book_id)
     return BOOKS.get(book_id)
 
+
 # Path Parameter GET
 @app.get("/books/{book_id}")
-async def read_book(book_id: int) -> str:
+async def read_book(book_id: int) -> dict[str, str]:
     validate_book_id(book_id)
     return BOOKS.get(book_id)
+
 
 # Basic POST
 @app.post("/")
@@ -45,6 +50,7 @@ async def create_book(book_title: str, book_author: str) -> str:
     book = {'title': book_title, 'author': book_author}
     BOOKS[book_id] = book
     return JSONResponse(status_code=202, content=book)
+
 
 # Basic PUT
 @app.put("/books/{book_id}")
@@ -56,6 +62,7 @@ async def update_book(book_id: int, book_title: str, book_author: str) -> str:
     BOOKS[book_id] = book
     return JSONResponse(status_code=202, content=book)
 
+
 # Basic DELETE
 @app.delete("/books/{book_id}")
 async def delete_book(book_id: int) -> str:
@@ -64,6 +71,7 @@ async def delete_book(book_id: int) -> str:
     del BOOKS[book_id]
     return JSONResponse(status_code=202, content=book)
 
+
 @app.delete("/books/")
 async def delete_book(book_id: int) -> str:
     validate_book_id(book_id)
@@ -71,7 +79,8 @@ async def delete_book(book_id: int) -> str:
     del BOOKS[book_id]
     return JSONResponse(status_code=202, content=book)
 
+
 # Helper Method
 def validate_book_id(book_id: int) -> None:
-    if (book_id > len(BOOKS) or book_id <= 0):
+    if book_id > len(BOOKS) or book_id <= 0:
         raise HTTPException(status_code=401, detail="Invalid Book Id")
